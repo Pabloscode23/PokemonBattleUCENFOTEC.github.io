e56e75bd65a61cb3c4723b15a12f4aeb05eceba7
Jose Pablo Navarro <jnavarror@ucenfotec.ac.cr>
2024-07-08 16:55:22 -0600

    Paginas

---
+++ assets/js/create-teams.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const resetButton = document.querySelector('.button__reset');
+    const teamNameInput = document.getElementById('team__name-title');
+    const checkIcon = document.getElementById('checkIcon');
+    const teamNameError = document.getElementById('teamNameError');
+
+    resetButton.addEventListener('click', () => {
+        teamNameInput.value = '';
+        validateTeamName();
+    });
+
+    teamNameInput.addEventListener('input', function () {
+        validateTeamName();
+    });
+
+    function validateTeamName() {
+        const teamName = teamNameInput.value.trim();
+        if (teamName.length < 8) {
+            teamNameError.textContent = 'El nombre del equipo debe tener al menos 8 caracteres.';
+            teamNameError.classList.add('error-message');
+            teamNameError.classList.remove('success-message');
+            team__name - title.classList.remove('valid');
+            team__name - title.classList.add('invalid');
+            return false;
+        } else {
+            teamNameError.textContent = 'Nombre válido.';
+            teamNameError.classList.remove('error-message');
+            teamNameError.classList.add('success-message');
+            team__name - title.classList.remove('invalid');
+            team__name - title.classList.add('valid');
+            return true;
+        }
+    }
+
+});
---
+++ assets/js/list-teams.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const playerInfoButton = document.querySelector('.button__player-info');
+    playerInfoButton.addEventListener('click', function () {
+        window.location.href = '../html/friends-profile.html';
+    });
+});
